b9762988c34d4d1396aeb663aea8a61060aa50e0
Douglas Duarte <douglasduarte.eng@gmail.com>
2024-02-10 11:26:50 -0300

    feat(new_note): add form to create new note

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,7 +1,35 @@
 import * as Dialog from "@radix-ui/react-dialog"
 import { X } from "lucide-react"
+import { ChangeEvent, useState } from "react"
+import { useForm } from "react-hook-form"
+import { toast } from 'sonner'
+
+interface NoteInputs {
+  content: string
+}
 
 export const NewNoteCard = () => {
+  const [shouldShowOnboarding, setShouldShowOnboarding] = useState<boolean>(true)
+  const newNote = useForm<NoteInputs>()
+
+  const { register, handleSubmit } = newNote
+
+  function handleStartEditor() {
+    setShouldShowOnboarding(false)
+  }
+
+  function handleContentChange(event: ChangeEvent<HTMLTextAreaElement>) {
+    const hasValue = event.target.value !== ""
+    if (!hasValue) {
+      setShouldShowOnboarding(true)
+    }
+  }
+
+  function handleSaveNote(data: NoteInputs) {
+    console.log(data)
+    console.log("Saving note...")
+    toast.success("Nota criada com sucesso!")
+  }
 
   return (
     <Dialog.Root>
@@ -10,7 +38,7 @@ export const NewNoteCard = () => {
           Adicionar nota
         </span>
         <p className="text-sm leading-6 text-slate-400">
-          Grave uma nota em áudio que será convertida para texto automaticamente
+          Grave uma nota em áudio que será convertida para texto automaticamente.
         </p>
       </Dialog.Trigger>
       <Dialog.Portal>
@@ -19,20 +47,33 @@ export const NewNoteCard = () => {
           <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
             <X className="size-5" />
           </Dialog.Close>
-          <div className="flex flex-1 flex-col gap-3 p-5">
-            <span className="text-sm font-medium text-slate-300">
-              Adicionar nota
-            </span>
-            <p className="text-sm leading-6 text-slate-400">
-              Comece <button className="text-lime-400 hover:underline">gravando uma nota</button> em áudio ou se preferir <button className="text-lime-400 hover:underline">utilize apenas texto</button>.
-            </p>
-          </div>
-          <button
-            className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
-            type="button"
-          >
-            Salvar nota
-          </button>
+          <form onSubmit={handleSubmit(handleSaveNote)} className="flex-1 flex flex-col">
+            <div className="flex flex-1 flex-col gap-3 p-5">
+              <span className="text-sm font-medium text-slate-300">
+                Adicionar nota
+              </span>
+              {
+                shouldShowOnboarding ? (
+                  <p className="text-sm leading-6 text-slate-400">
+                    Comece <button className="text-lime-400 hover:underline">gravando uma nota</button> em áudio ou se preferir <button onClick={handleStartEditor} className="text-lime-400 hover:underline">utilize apenas texto</button>.
+                  </p>
+                ) : (
+                  <textarea
+                    {...register("content")}
+                    autoFocus
+                    className="text-sm leading-6 text-slate-400 bg-transparent outline-none resize-none flex-1"
+                    onChange={handleContentChange}
+                  />
+                )
+              }
+            </div>
+            <button
+              className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+              type="submit"
+            >
+              Salvar nota
+            </button>
+          </form>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
+import { Toaster } from 'sonner'
 
 import "./main.css"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Toaster richColors />
   </React.StrictMode>,
 )
