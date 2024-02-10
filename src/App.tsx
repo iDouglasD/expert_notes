bd3fa69c2bb7df04af38cab7d86866f84eca4f78
Douglas Duarte <douglasduarte.eng@gmail.com>
2024-02-10 09:17:06 -0300

    feat(home): finished layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
+import logo from "./assets/logo-nlw-expert.svg"
+import { NewNotCard } from "./components/new-note-card.tsx"
+import { NoteCard } from "./components/note-card.tsx"
 
 export function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline text-red-700">
-      Hello world!
-    </h1>
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} alt="NLW expert" />
+      <form className="w-full">
+        <input
+          className="w-full bg-transparent text-3xl font-semibold tracking-tighter outline-none placeholder:text-slate-500"
+          type="text"
+          placeholder="Busque em suas notas..."
+        />
+      </form>
+      <div className="h-px bg-slate-700" />
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNotCard />
+        <NoteCard />
+      </div>
+    </div>
   )
 }
 
